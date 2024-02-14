50429fae072f20c1d715afaab3f45548b36550f8
kr3pt0n <egor20042013@gmail.com>
2024-02-14 13:46:04 +0100

    done

--- generator of passwords/Form1.Designer.cs
+++ generator of passwords/Form1.Designer.cs
@@ -29,13 +29,135 @@ namespace generator_of_passwords
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            outTextBox = new System.Windows.Forms.TextBox();
+            generateBut = new System.Windows.Forms.Button();
+            LangComboBox = new System.Windows.Forms.ComboBox();
+            NumbersBox = new System.Windows.Forms.CheckBox();
+            TextLittleBox = new System.Windows.Forms.CheckBox();
+            TextBigBox = new System.Windows.Forms.CheckBox();
+            SpecialSymbolsBox = new System.Windows.Forms.CheckBox();
+            LenghtLabel = new System.Windows.Forms.Label();
+            numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // outTextBox
+            // 
+            outTextBox.Location = new System.Drawing.Point(143, 59);
+            outTextBox.Name = "outTextBox";
+            outTextBox.ReadOnly = true;
+            outTextBox.Size = new System.Drawing.Size(200, 23);
+            outTextBox.TabIndex = 0;
+            // 
+            // generateBut
+            // 
+            generateBut.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            generateBut.Location = new System.Drawing.Point(143, 287);
+            generateBut.Name = "generateBut";
+            generateBut.Size = new System.Drawing.Size(200, 50);
+            generateBut.TabIndex = 1;
+            generateBut.Text = "Сгенерировать пароль";
+            generateBut.UseVisualStyleBackColor = true;
+            generateBut.Click += generateBut_Click;
+            // 
+            // LangComboBox
+            // 
+            LangComboBox.FormattingEnabled = true;
+            LangComboBox.Items.AddRange(new object[] { "Русский ", "Українська", "English", "Espanol" });
+            LangComboBox.Location = new System.Drawing.Point(361, 12);
+            LangComboBox.Name = "LangComboBox";
+            LangComboBox.Size = new System.Drawing.Size(111, 23);
+            LangComboBox.TabIndex = 2;
+            LangComboBox.SelectedIndexChanged += LangComboBox_SelectedIndexChanged;
+            // 
+            // NumbersBox
+            // 
+            NumbersBox.AutoSize = true;
+            NumbersBox.Location = new System.Drawing.Point(20, 152);
+            NumbersBox.Name = "NumbersBox";
+            NumbersBox.Size = new System.Drawing.Size(83, 19);
+            NumbersBox.TabIndex = 3;
+            NumbersBox.Text = "checkBox1";
+            NumbersBox.UseVisualStyleBackColor = true;
+            // 
+            // TextLittleBox
+            // 
+            TextLittleBox.AutoSize = true;
+            TextLittleBox.Location = new System.Drawing.Point(20, 177);
+            TextLittleBox.Name = "TextLittleBox";
+            TextLittleBox.Size = new System.Drawing.Size(83, 19);
+            TextLittleBox.TabIndex = 4;
+            TextLittleBox.Text = "checkBox2";
+            TextLittleBox.UseVisualStyleBackColor = true;
+            // 
+            // TextBigBox
+            // 
+            TextBigBox.AutoSize = true;
+            TextBigBox.Location = new System.Drawing.Point(20, 202);
+            TextBigBox.Name = "TextBigBox";
+            TextBigBox.Size = new System.Drawing.Size(83, 19);
+            TextBigBox.TabIndex = 5;
+            TextBigBox.Text = "checkBox3";
+            TextBigBox.UseVisualStyleBackColor = true;
+            // 
+            // SpecialSymbolsBox
+            // 
+            SpecialSymbolsBox.AutoSize = true;
+            SpecialSymbolsBox.Location = new System.Drawing.Point(20, 227);
+            SpecialSymbolsBox.Name = "SpecialSymbolsBox";
+            SpecialSymbolsBox.Size = new System.Drawing.Size(83, 19);
+            SpecialSymbolsBox.TabIndex = 6;
+            SpecialSymbolsBox.Text = "checkBox4";
+            SpecialSymbolsBox.UseVisualStyleBackColor = true;
+            // 
+            // LenghtLabel
+            // 
+            LenghtLabel.AutoSize = true;
+            LenghtLabel.Location = new System.Drawing.Point(20, 105);
+            LenghtLabel.Name = "LenghtLabel";
+            LenghtLabel.Size = new System.Drawing.Size(153, 15);
+            LenghtLabel.TabIndex = 7;
+            LenghtLabel.Text = "Длина желаемого пароля:";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new System.Drawing.Point(66, 123);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new System.Drawing.Size(50, 23);
+            numericUpDown1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(484, 361);
+            Controls.Add(numericUpDown1);
+            Controls.Add(LenghtLabel);
+            Controls.Add(SpecialSymbolsBox);
+            Controls.Add(TextBigBox);
+            Controls.Add(TextLittleBox);
+            Controls.Add(NumbersBox);
+            Controls.Add(LangComboBox);
+            Controls.Add(generateBut);
+            Controls.Add(outTextBox);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox outTextBox;
+        private System.Windows.Forms.Button generateBut;
+        private System.Windows.Forms.ComboBox LangComboBox;
+        private System.Windows.Forms.CheckBox NumbersBox;
+        private System.Windows.Forms.CheckBox TextLittleBox;
+        private System.Windows.Forms.CheckBox TextBigBox;
+        private System.Windows.Forms.CheckBox SpecialSymbolsBox;
+        private System.Windows.Forms.Label LenghtLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- generator of passwords/Form1.cs
+++ generator of passwords/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,7 +17,152 @@ namespace generator_of_passwords
         public Form1()
         {
             InitializeComponent();
+            LangComboBox.SelectedIndex = 0;
+            LangComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        private void LangComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int langindext = LangComboBox.SelectedIndex + 1;
+            switch (langindext)
+            {
+                case 1:
+                    {
+                        LenghtLabel.Text = "Длина желаемого пароля:";
+                        NumbersBox.Text = "Цифры";
+                        TextLittleBox.Text = "Строчные буквы";
+                        TextBigBox.Text = "Заглавные буквы";
+                        SpecialSymbolsBox.Text = "Специальные символы";
+                        generateBut.Text = "Сгенерировать пароль";
+                        break;
+                    }
+                case 2:
+                    {
+                        LenghtLabel.Text = "Довжина бажаного паролю:";
+                        NumbersBox.Text = "Цифри";
+                        TextLittleBox.Text = "Малі літери";
+                        TextBigBox.Text = "Великі літери";
+                        SpecialSymbolsBox.Text = "Спеціальні символи";
+                        generateBut.Text = "Згенерувати пароль";
+                        break;
+                    }
+                case 3:
+                    {
+                        LenghtLabel.Text = "Desired Password Length:";
+                        NumbersBox.Text = "Numbers";
+                        TextLittleBox.Text = "Lowercase Letters";
+                        TextBigBox.Text = "Uppercase Letters";
+                        SpecialSymbolsBox.Text = "Special Symbols";
+                        generateBut.Text = "Generate Password";
+                        break;
+                    }
+                case 4:
+                    {
+                        LenghtLabel.Text = "Longitud de la contraseña deseada:";
+                        NumbersBox.Text = "Numeros";
+                        TextLittleBox.Text = "Letras minusculas";
+                        TextBigBox.Text = "Letras mayusculas";
+                        SpecialSymbolsBox.Text = "Simbolos especiales";
+                        generateBut.Text = "Generar contraseña";
+                        break;
+                    }
+            }
         }
 
+        private void generateBut_Click(object sender, EventArgs e)
+        {
+            int checkall = 0;
+            List<int> ItemList = new List<int>();
+            if (NumbersBox.Checked)
+            {
+                ItemList.AddRange(new List<int> { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57 });
+                checkall++;
+            }
+            if (TextLittleBox.Checked)
+            {
+                ItemList.AddRange(new List<int> { 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122 });
+                checkall++;
+            }
+            if (TextBigBox.Checked)
+            {
+                ItemList.AddRange(new List<int> { 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90 });
+                checkall++;
+            }
+            if (SpecialSymbolsBox.Checked)
+            {
+                ItemList.AddRange(new List<int> { 36, 37, 94, 38, 40, 41, 45, 95, 61, 43, 91, 93, 123, 125, 59, 58, 44, 46, 47, 92, 124, 39, 34, 60, 62, 63 });
+                checkall++;
+            }
+            if (checkall < 1)
+            {
+                int langindext = LangComboBox.SelectedIndex + 1;
+                switch (langindext)
+                {
+                    case 1:
+                        {
+                            MessageBox.Show("Вы должны отметить хотя бы один пункт для создания пароля.", "Ошибка.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        }
+                    case 2:
+                        {
+                            MessageBox.Show("Ви повинні відмітити принаймні один пункт для створення пароля.", "Помилка.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        }
+                    case 3:
+                        {
+                            MessageBox.Show("You must check at least one item to create a password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        }
+                    case 4:
+                        {
+                            MessageBox.Show("Debe marcar al menos un elemento para crear una contraseña.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        }
+
+                }
+
+            }
+            Random random = new Random();
+            for (int i = 0; i < ItemList.Count; i++)
+            {
+                int randomIndex = random.Next(i, ItemList.Count);
+                int temp = ItemList[i];
+                ItemList[i] = ItemList[randomIndex];
+                ItemList[randomIndex] = temp;
+            }
+
+            int arraySize = Convert.ToInt32(numericUpDown1.Value);
+            int[] password = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                try
+                {
+                    int randomInt = random.Next(1, ItemList.Count);
+                    password[i] = ItemList[randomInt];
+                }
+                catch { }
+                Debug.Write(password[i] + " ");
+            }
+            string FinalPassword = ConvertAsciiCodesToString(password);
+            outTextBox.Text = FinalPassword;
+
+
+
+
+            ItemList.Clear();
+        }
+        static string ConvertAsciiCodesToString(int[] asciiCodes)
+        {
+            StringBuilder resultStringBuilder = new StringBuilder();
+
+            foreach (int code in asciiCodes)
+            {
+                char character = Convert.ToChar(code);
+                resultStringBuilder.Append(character);
+            }
+
+            return resultStringBuilder.ToString();
+        }
     }
 }
+
